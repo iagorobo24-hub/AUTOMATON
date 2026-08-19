5df2f9f139cf227966cb933ffd9d3ce9093c047a
Iago Durán Romera <iagorobo24@gmail.com>
2026-08-19 20:13:12 +0200

    feat: add backtesting package

---
+++ backend/app/backtesting/__init__.py
@@ -0,0 +1,3 @@
+from app.backtesting.datasets import DatasetValidationError, canonical_dataset_sha256, persist_dataset
+
+__all__ = ["DatasetValidationError", "canonical_dataset_sha256", "persist_dataset"]
